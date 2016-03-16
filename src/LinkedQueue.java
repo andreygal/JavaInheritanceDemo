a3a9a0f78d5ef8c1c0afb4d1de914dc8af1224d0
andreygal <andreygalper@gmail.com>
2016-03-16 13:40:08 -0400

    Implementing the queue class from scratch

---
+++ src/LinkedQueue.java
@@ -0,0 +1,6 @@
+/**
+ * Created by smertmashina on 3/16/16.
+ */
+public class LinkedQueue<T> implements Queue<T> {
+
+}
--- src/Queue.java
+++ src/Queue.java
@@ -1,5 +1,13 @@
 /**
- * Created by smertmashina on 3/15/16.
+ * Created by smertmashina on 3/16/16.
  */
-public class Queue {
+public interface Queue<T> {
+    //add an element to the back of the queue
+    void push(T element);
+    //remove an element from the front of the queue
+    void pop();
+    //access an element at the front of the queue
+    T front();
+    //test if the queue is empty
+    Boolean isEmpty();
 }
